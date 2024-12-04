4c2eb33525e5c51f9532f708efe38ec595baac2d
Brian McMillen <brianmcmillen1@gmail.com>
2024-12-03 20:52:14 -0500

    day 3 part 2 solve

--- day3.php
+++ day3.php
@@ -1,21 +1,29 @@
 <?php
-$lines = file('./day3.txt');
+$contents = file_get_contents('./day3.txt');
 
 $data = [];
 $regex = "/mul\(\d+,\d+\)/";
 $total = 0;
-foreach ($lines as $line) {
-  if (preg_match_all($regex, $line, $matches)) {
-    foreach ($matches[0] as $match) {
-      preg_match_all("/\d+/", $match, $nums);
-      $numA = $nums[0][0];
-      $numB = $nums[0][1];
-      $total += $numA * $numB;
-    }
+if (preg_match_all($regex, $contents, $matches)) {
+  foreach ($matches[0] as $match) {
+    preg_match_all("/\d+/", $match, $nums);
+    $numA = $nums[0][0];
+    $numB = $nums[0][1];
+    $total += $numA * $numB;
   }
 }
 
-
 echo "Part 1: $total \n";
+
+$total = 0;
+$goodContents = preg_replace(["/\R+/", "/don't\(\).*?do\(\)/"], '', $contents);
+if (preg_match_all($regex, $goodContents, $matches)) {
+  foreach ($matches[0] as $match) {
+    preg_match_all("/\d+/", $match, $nums);
+    $numA = $nums[0][0];
+    $numB = $nums[0][1];
+    $total += $numA * $numB;
+  }
+}
 
 echo "Part 2: $total \n";
