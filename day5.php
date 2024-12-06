0217d5392d07c4c11abcfd6aef84fa390d0601b5
Brian McMillen <brianmcmillen1@gmail.com>
2024-12-06 08:36:27 -0500

    day 5 part 1 solve

--- day5.php
+++ day5.php
@@ -1,17 +1,68 @@
 <?php
-$contents = file_get_contents('./day5.txt');
+$lines = file('./day5.txt');
+
+$rules = [];
+$updates = [];
+$lookingAtRules = true;
+foreach ($lines as $line) {
+  if (strlen($line) < 2) {
+    $lookingAtRules = false;
+    continue;
+  }
+
+  $line = preg_replace('/\R/', '', $line);
+
+  if ($lookingAtRules) {
+    $rules[] = explode('|', $line);
+  } else {
+    $updates[] = $line;
+  }
+}
 
 $total = 0;
-$len = strpos($contents, "\n");
-$left = $len - 1;
-$right = $len + 1;
+$badUpdates = [];
+foreach ($updates as $update) {
+  foreach ($rules as $rule) { 
+    $regex = '/,' . implode(',.*,', array_reverse($rule)) . ',/';
+    if (preg_match($regex, "," . $update . ",")) {
+      $badUpdates[] = $update;
+      continue 2;
+    }
+  }
+
+  $parts = explode(',', $update);
+  $len = count($parts);
+  $index = ($len - 1) / 2;
+  $total += $parts[($len - 1) / 2];
+}
 
 echo "Part 1: $total \n";
 
+$goodUpdates = [];
 $total = 0;
-// $total += preg_match_all("/(?=(M.M(?:.|\R){" . $left . "}A(?:.|\R){" . $left . "}S.S))/", $contents);
-// $total += preg_match_all("/(?=(S.S(?:.|\R){" . $left . "}A(?:.|\R){" . $left . "}M.M))/", $contents);
-// $total += preg_match_all("/(?=(M.S(?:.|\R){" . $left . "}A(?:.|\R){" . $left . "}M.S))/", $contents);
-// $total += preg_match_all("/(?=(S.M(?:.|\R){" . $left . "}A(?:.|\R){" . $left . "}S.M))/", $contents);
+foreach ($badUpdates as $update) {
+  foreach ($rules as $rule) {
+    $regex = '/,' . implode(',.*,', array_reverse($rule)) . ',/';
+    if (preg_match($regex, "," . $update . ",")) {
+      $parts = explode(',', $update);
+      $indexA = array_search($rule[0], $parts);
+      $indexB = array_search($rule[1], $parts);
+      // This isn't working, just swapping the numbers might mess up previously
+      // fixed rules
+      print_r($rule[0] . "\n");
+      $parts[$indexA] = $rule[1];
+      print_r($rule[1] . "\n");
+      $parts[$indexB] = $rule[0];
+      print_r($update . "\n");
+      $update = join(',', $parts);
+      print_r($update . "\n");
+    }
+  }
+
+  $parts = explode(',', $update);
+  $len = count($parts);
+  $index = ($len - 1) / 2;
+  $total += $parts[($len - 1) / 2];
+}
 
 echo "Part 2: $total \n";
